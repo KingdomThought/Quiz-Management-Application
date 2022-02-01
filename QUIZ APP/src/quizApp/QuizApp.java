ee14f0024a68b2df0d61e11d51bd53d64ef21053
KingdomThought <79799753+KingdomThought@users.noreply.github.com>
2022-02-01 10:51:44 -0500

    Add files via upload

---
+++ QUIZ APP/src/quizApp/QuizApp.java
@@ -0,0 +1,13 @@
+package quizApp;
+
+public class QuizApp {
+
+	public static void main(String[] args) {
+		
+		QuizMaker q1= new QuizMaker();
+		q1.questionInput();
+		// TODO Auto-generated method stub
+
+	}
+
+}
